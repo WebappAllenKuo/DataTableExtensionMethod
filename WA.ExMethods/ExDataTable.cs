3801f0ed9365d760d0e4b6fb87d01380354cb561
Allen Kuo <g01.webapp@gmail.com>
2020-04-07 21:53:46 +0800

    feat : add DataTable extention method - WA_AddNewRow

--- WA.ExMethods/ExDataTable.cs
+++ WA.ExMethods/ExDataTable.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WA.ExMethods
 {
-    public class ExDataTable
+	/// <summary>
+	/// DataTable 的擴充方法
+	/// </summary>
+    public static class ExDataTable
     {
+		/// <summary>
+		/// 為 DataTable 加入一筆新記錄
+		/// </summary>
+		/// <param name="dataTable"></param>
+		/// <param name="addNewRowAction"></param>
+	    public static void WA_AddNewRow(this DataTable dataTable, Action<DataRow> assignColumnsValue)
+	    {
+		    var newRow = dataTable.NewRow();
+		    assignColumnsValue(newRow);
+		    dataTable.Rows.Add(newRow);
+	    }
     }
 }
