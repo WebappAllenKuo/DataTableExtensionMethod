9d4c1626c4793cc49f018f37d60edfbd6f89fd83
Allen Kuo <g01.webapp@gmail.com>
2020-04-07 21:58:30 +0800

    feat: add UnitTest

---
+++ WA.ExMethods.Tests/ExDataTableTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace WA.ExMethods.Tests
+{
+    [TestFixture]
+    public class ExDataTableTests
+    {
+	    [Test]
+	    public void WA_AddNewRow_WhenCalled()
+	    {
+			DataTable dataTable = new DataTable();
+			dataTable.Columns.Add("Id", typeof(int));
+			dataTable.Columns.Add("Name", typeof(string));
+
+			dataTable.WA_AddNewRow(row =>
+			{
+				row["Id"] = 1;
+				row["Name"] = "Allen";
+			});
+
+			dataTable.Rows.Count.Should().Be(1);
+	    }
+    }
+}
